bdd6382c8e134d91bab9ca4d7e8a9a59d13e505a
sin392 <sin392@gmail.com>
2020-10-05 16:08:41 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template(
+        "index.html",
+        title="index"
+        )
+
+@app.route("/research", methods=["GET"])
+def research():
+    return render_template(
+        "research.html",
+        title="research"
+        )
+
+@app.route("/member", methods=["GET"])
+def member():
+    return render_template(
+        "member.html",
+        title="member"
+        )
+
+if __name__ == "__main__":
+    app.run(debug=True)
